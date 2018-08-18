73d71cd4f2e10a9223d31fd55bf368db854812ea
Jian James Astrero <jianjamesastrero@gmail.com>
2018-08-18 22:26:40 +0800

    Add temporary response

--- app/Http/Controllers/WebHookController.php
+++ app/Http/Controllers/WebHookController.php
@@ -3,11 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use function json_encode;
 
 class WebHookController extends Controller
 {
     public function getSickness(Request $request) {
-        return json_encode($request->toArray());
+        return "Hello from WebHook!";
     }
 }
