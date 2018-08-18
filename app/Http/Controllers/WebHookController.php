3e61769e7b03ddbe62c0dd2661309a27cc723a03
Jian James Astrero <jianjamesastrero@gmail.com>
2018-08-19 00:09:41 +0800

    display symptoms

--- app/Http/Controllers/WebHookController.php
+++ app/Http/Controllers/WebHookController.php
@@ -7,8 +7,9 @@ use Illuminate\Http\Request;
 class WebHookController extends Controller
 {
     public function getSickness(Request $request) {
+        $symptoms = $request->queryResult->symptom;
         $response = array(
-            "fulfillmentText" => 'Hello from webhook',
+            "fulfillmentText" => 'Hello from webhook, your symptoms are: '.json_decode($symptoms),
         );
         return json_encode($response);
     }
