242ceb43df6ed84bcfc62d99b9dd0082003c2734
Jian James Astrero <jianjamesastrero@gmail.com>
2018-08-19 00:27:13 +0800

    display possible sickness

--- app/Http/Controllers/WebHookController.php
+++ app/Http/Controllers/WebHookController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Sickness;
+use App\Symptom;
 use Illuminate\Http\Request;
 
 class WebHookController extends Controller
@@ -9,8 +11,16 @@ class WebHookController extends Controller
     public function getSickness(Request $request) {
         $json = $request->json()->all();
         $symptoms = $json['queryResult']['parameters']['symptom'];
+        $possibleSickness = array();
+        foreach ($symptoms as $symptom) {
+            $items = Symptom::where('name', $symptom)->get();
+            foreach ($items as $item) {
+                $sickness = $item->sickness();
+                $possibleSickness[] = $sickness->name;
+            }
+        }
         $response = array(
-            "fulfillmentText" => json_encode($symptoms),
+            "fulfillmentText" => json_encode($possibleSickness),
         );
         return json_encode($response);
     }
