d2dae7315b1e80add5fe7fe92568b8bfd868de1f
Jian James Astrero <jianjamesastrero@gmail.com>
2018-08-19 02:03:16 +0800

    Show possible sickness in laymans term

--- app/Http/Controllers/WebHookController.php
+++ app/Http/Controllers/WebHookController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Sickness;
 use App\Symptom;
+use function count;
 use Illuminate\Http\Request;
 use function value;
 
@@ -23,8 +24,33 @@ class WebHookController extends Controller
                 $possibleSickness[] = $sickness;
             }
         }
+        $sicknessScores = array();
+        foreach ($possibleSickness as $item) {
+            if (array_key_exists($item, $possibleSickness)) {
+                $sicknessScores[$item] = 1;
+            } else {
+                $sicknessScores[$item] = $sicknessScores[$item] + 1;
+            }
+        }
+        $fulfillmentText = "You are healthy. Your symptoms are probably just coincidence, or Something i may not know of.";
+        $possibleSickness = array_unique($possibleSickness);
+        if (count($possibleSickness) == 1) {
+            $fulfillmentText = "You have $possibleSickness[0]";
+        } else if ($possibleSickness > 1) {
+            $fulfillmentText = "You most likely have $possibleSickness[0], but you are in risk of having ";
+            if (count($possibleSickness)-1 > 1) {
+                $i = 0;
+                for (; $i < count($possibleSickness)-2; $i++) {
+                        $fulfillmentText .= $possibleSickness[$i].', ';
+                }
+                $fulfillmentText .= 'and '.$possibleSickness[$i];
+            } else {
+                $fulfillmentText .= $possibleSickness[1];
+            }
+            $fulfillmentText .= 'too';
+        }
         $response = array(
-            "fulfillmentText" => json_encode($possibleSickness),
+            "fulfillmentText" => $fulfillmentText,
         );
         return json_encode($response);
     }
